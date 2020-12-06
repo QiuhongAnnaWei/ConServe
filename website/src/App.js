3ed75f97644c786da064a1d960983b4e7ed833e2
livia01px2019 <livia.zhu@live.com>
2020-12-06 21:19:24 +0800

    app is no longer function

--- website/src/App.js
+++ website/src/App.js
@@ -15,20 +15,21 @@ import { Page as GroceryPage } from './pages/GroceryList';
 import { RecipesPage } from './pages/Recipes';
 import Header from './components/Header';
 
-function App() {
-
-  return (
-    <div>
-      <Router>
-        <Header></Header>
-        <Switch>
-          <Route exact path="/" component={GroceryPage}></Route>
-          <Route exact path="/groceries" component={GroceryPage}></Route>
-          <Route exact path="/recipes" component={RecipesPage}></Route>
-        </Switch>
-      </Router>
-    </div>
-  );
+class App extends React.Component {
+  render() {
+    return (
+      <div>
+        <Router>
+          <Header></Header>
+          <Switch>
+            <Route exact path="/" component={GroceryPage}></Route>
+            <Route exact path="/groceries" component={GroceryPage}></Route>
+            <Route exact path="/recipes" component={RecipesPage}></Route>
+          </Switch>
+        </Router>
+      </div>
+    );
+  }
 }
 
 export default App;
