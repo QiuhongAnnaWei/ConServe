cd20ffe393b59f2906f9c5b48e7ec25e583a14d8
livia01px2019 <livia.zhu@live.com>
2020-12-06 16:21:04 +0800

    add groceryList css

--- website/src/pages/GroceryList.js
+++ website/src/pages/GroceryList.js
@@ -1,9 +1,10 @@
 import React, { Component } from "react";
+import './GroceryList.css';
 
 export class Page extends React.Component {
     render() {
         return (
-            <div>
+            <div className="groceryListBody">
                 grocery
             </div>
         )
